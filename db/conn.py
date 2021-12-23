82a14013b3ca20f59a7e580d3839e2b3ef9e610e
jess <jess.monter@ciencias.unam.mx>
2021-12-22 18:39:00 -0600

    Add employee deletion implementation and helper functions

--- db/conn.py
+++ db/conn.py
@@ -20,14 +20,35 @@ def consulta_empleado(empleado_id):
     result = [dict(zip(column_names, record)) for record in records][0]
     return result
 
+# TODO: Implementar la función de inserción de un nuevo empleado
+# args: Los atributos del empleado
+# return: Los datos del empleado {"id": id, "nombre": nombre ...}
 def inserta_empleado():
+    # Generar la consulta con los argumentos
+    # Ejecutar la consulta
+    # Traer los datos de la ejecución de la consulta (empleado creado)
+    # Generar el diccionario con los datos del empleado para regresarlo
     pass
 
 def actualiza_empleado():
     pass
 
-def elimina_empleado(empleado_id):
-    # TODO: Implementar funcion
-    consulta = ""
-    cursor.execute(consulta)
 
+def elimina_empleado(empleado_id):
+    tablas_relacionadas = ["contacto", "direccion", "documento_empleado", "empresa_empleado"]
+
+    for tabla in tablas_relacionadas:
+        consulta_relacionada = construye_consulta_borrado(tabla, "empleado_id", empleado_id)
+        realiza_consulta(consulta_relacionada)
+
+    realiza_consulta(construye_consulta_borrado("empleado", "id", empleado_id))
+
+
+# TODO: ¿Este es buen lugar para escribir estas funciones?
+def construye_consulta_borrado(tabla, columna, valor_columna):
+    consulta = f"DELETE FROM {tabla} WHERE {columna}={valor_columna}"
+    return consulta
+
+def realiza_consulta(consulta):
+    cursor.execute(consulta)
+    conn.commit()
